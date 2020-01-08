d9d79a1a895d6638be40a14285dfd9fdcef39a8c
Heli Sivunen <heli@mooncat.tv>
2020-01-08 09:34:15 +0000

    first commit

---
+++ Yes_or_no_app.UITests/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Yes_or_no_app.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
